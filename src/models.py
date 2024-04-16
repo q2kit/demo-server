1bc3dcf624eb45a52bb5e99525112cbc5b490335
Q2k <q2k@q2k.dev>
2024-04-16 15:31:34 +0700

    [update] not allow assign project to admin

--- src/models.py
+++ src/models.py
@@ -6,7 +6,11 @@ from .funks import gen_secret
 
 class Project(models.Model):
     domain = models.CharField(max_length=255, unique=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        limit_choices_to={'is_superuser': False},
+    )
     secret = models.CharField(max_length=255, default=gen_secret)
 
     def __str__(self) -> str:
