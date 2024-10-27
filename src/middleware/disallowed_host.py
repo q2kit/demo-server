309c4f6c4bece32a2bb4f4398d280940604a44f1
Q2k <q2k@q2k.dev>
2024-10-27 12:59:32 +0700

    fix: handle disallowhost error

---
+++ src/middleware/disallowed_host.py
@@ -0,0 +1,11 @@
+from django.utils.deprecation import MiddlewareMixin
+from django.core.exceptions import DisallowedHost
+from django.http import HttpResponse
+
+
+class DisallowedHostMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        try:
+            request.get_host()
+        except DisallowedHost:
+            return HttpResponse(status=400)
